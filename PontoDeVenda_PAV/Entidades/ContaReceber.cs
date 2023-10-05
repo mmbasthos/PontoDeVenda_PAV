8940689af8e7978a5b659fb8995356525bcdd7b2
AlexVilla11 <lex.villa1106@gmail.com>
2023-10-05 08:50:47 -0300

    ContaReceber Exemplo

---
+++ PontoDeVenda_PAV/Entidades/ContaReceber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PontoDeVenda_PAV.Entidades
+{
+    internal class ContaReceber
+    {
+    }
+}
+
+
+/*
+Table: conta_receber
+Columns:
+id_conta_receber int AI PK 
+descricao_receber varchar(200) 
+data_lancamento date 
+data_vencimento date 
+valor_total decimal(10,2) 
+valor_recebido decimal(10,2) 
+data_recebimento date 
+valor_recebimento decimal(10,2) 
+Cliente_id_cliente int
+*/
